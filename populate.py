06a936c512ba5ed509319e2f0ea2cf2097ac151f
Quint Guvernator <quint@guvernator.net>
2024-03-19 18:35:19 +0100

    populate: print created filenames

--- populate.py
+++ populate.py
@@ -1,7 +1,8 @@
+import pathlib
+import sys
+
 import requests
 import yaml
-
-import pathlib
 
 script_path = pathlib.Path(__file__).resolve().parent
 features_path = script_path / 'features'
@@ -14,15 +15,16 @@ def create_features(name, data):
             path.parent.mkdir(parents=True, exist_ok=True)
             with path.open('w') as f:
                 yaml.dump(dict(id=name, support='unknown'), f)
+                print(path.relative_to(features_path.parent), file=sys.stderr)
 
     for k, v in data.items():
         if k != '__compat':
             create_features(f'{name}.{k}', v)
 
 root = requests.get('https://unpkg.com/@mdn/browser-compat-data@5.5.16/data.json').json()
-create_features('api', root['api'])
-create_features('html.elements', root['html']['elements'])
-create_features('html.global_attributes', root['html']['global_attributes'])
-create_features('http.data-url', root['http']['data-url'])
-create_features('http.headers', root['http']['headers'])
-create_features('javascript', root['javascript'])
+create_features('api', root['api']),
+create_features('html.elements', root['html']['elements']),
+create_features('html.global_attributes', root['html']['global_attributes']),
+create_features('http.data-url', root['http']['data-url']),
+create_features('http.headers', root['http']['headers']),
+create_features('javascript', root['javascript']),
