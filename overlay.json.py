755432c69c584dbebc5d009826b03b84f2ca6130
Quint Guvernator <quint@guvernator.net>
2024-03-21 19:48:24 +0100

    new format for overlay script

--- overlay.json.py
+++ overlay.json.py
@@ -3,17 +3,54 @@
 Determine which MDN pages should be altered to include data on Surfly support.
 '''
 
-# external
-import frontmatter
-
-# stdlib
+import enum
 import json
 import pathlib
 import sys
 
+import frontmatter
+
 cwd = pathlib.Path(__file__).parent
 mdn_path = cwd / 'content/files/en-us/web'
-surfly_path = cwd / 'surfly.json'
+surfly_path = cwd / 'pages-cms-test/features'
+
+
+class Support(enum.IntEnum):
+    UNKNOWN = 0
+    TESTED = 1
+    EXPECTED = 2
+    PARTIAL = 3
+    TODO = 4
+    NEVER = 5
+
+
+class Tree:
+    def __init__(self, v=None):
+        self.children = dict()
+        self.value = v
+
+    def __setitem__(self, path, v):
+        k, _, path = path.partition('.')
+        if not path:
+            self.children[k] = Tree(v)
+            return
+
+        if k not in self.children:
+            self.children[k] = Tree()
+        self.children[k][path] = v
+
+    def __getitem__(self, path):
+        k, _, path = path.partition('.')
+        if not path:
+            return self.children[k]
+
+        return self.children[k][path]
+
+    def __iter__(self):
+        if self.value is not None:
+            yield self.value
+        for child in self.children.values():
+            yield from child
 
 
 def parse_mdn():
@@ -27,26 +64,46 @@ def parse_mdn():
         except KeyError:
             continue
 
-        if type(features) != list:
+        if not isinstance(features, list):
             features = [features]
 
         path = fm['slug']
         yield (path, features)
 
 
-def only_important(mdn):
-    with surfly_path.open('r') as f:
-        surfly = json.load(f)
-        surfly_features = set(surfly.keys())
+def parse_support():
+    support_tree = Tree()
+    for path in surfly_path.glob("**/*.html"):
+        fm = frontmatter.load(path)
+        support_tree[fm['id']] = Support[fm['support'].upper()]
+    return support_tree
 
+
+def gen_overlay(*, mdn, support_tree):
     for path, features in mdn:
+        support_tables = []
+        for feature in features:
+            try:
+                support_table = list(support_tree[feature])
 
-        # if we have no data on this feature, skip
-        if set(features).isdisjoint(surfly_features):
+            # error in mozilla site source; skip this table
+            except KeyError:
+                continue
+
+            # make sparse
+            if all(support_row == Support.UNKNOWN for support_row in support_table):
+                support_table = Support.UNKNOWN
+
+            support_tables.append(support_table)
+
+        # make sparse
+        if all(support_table == Support.UNKNOWN for support_table in support_tables):
             continue
 
-        yield path, [surfly.get(feature) for feature in features]
+        yield (path, support_tables)
 
 
-overlay = only_important(parse_mdn())
-json.dump(dict(overlay), sys.stdout, separators=',:')
+mdn = parse_mdn()
+support_tree = parse_support()
+overlay = dict(gen_overlay(mdn=mdn, support_tree=support_tree))
+json.dump(overlay, sys.stdout, separators=',:')
