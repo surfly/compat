f492eb540f43eeed750dbee01e5158baaa22623b
Quint Guvernator <quint@guvernator.net>
2024-04-08 17:41:02 +0200

    ruff format

--- overlay.json.py
+++ overlay.json.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """Generate browser compatibility API data to be requested from MDN pages."""
 
+import copy
 import json
 import pathlib
 import sys
-import copy
 
 import frontmatter
 
@@ -16,13 +16,13 @@ surfly_path = root_path / "features"
 default_output_path = root_path / "scd"
 
 supported_browser_ids = [
-    'chrome',
-    'chrome_android',
-    'edge',
-    'firefox',
-    'firefox_android',
-    'safari',
-    'safari_ios',
+    "chrome",
+    "chrome_android",
+    "edge",
+    "firefox",
+    "firefox_android",
+    "safari",
+    "safari_ios",
 ]
 
 
@@ -40,16 +40,15 @@ def overlay(bcd_data, supported_browser_ids):
         icf_support = Support[icf_support_raw.upper()] if icf_support_raw else support
 
         feature = bcd.get_feature(bcd_data, feature_id)
-        native_browser_supports = feature['support']
-        feature['support'] = {}
+        native_browser_supports = feature["support"]
+        feature["support"] = {}
 
         for browser_id in supported_browser_ids:
-
             if browser_id not in native_browser_supports:
                 continue
 
             # carry over original support data from native browser
-            feature['support'][browser_id] = native_browser_supports[browser_id]
+            feature["support"][browser_id] = native_browser_supports[browser_id]
 
             # create "Surfly browser" column: start with a copy of the native browser support data
             surfly_support_entries = create_surfly_support_entries(
@@ -60,22 +59,24 @@ def overlay(bcd_data, supported_browser_ids):
                 icf_limitations,
                 extra_note,
             )
-            feature['support'][f'surfly_{browser_id}'] = surfly_support_entries
+            feature["support"][f"surfly_{browser_id}"] = surfly_support_entries
 
 
 def capitalize(s):
-    '''capitalize first word without lowercasing subsequent words'''
-    words = s.split(' ', maxsplit=1)
+    """capitalize first word without lowercasing subsequent words"""
+    words = s.split(" ", maxsplit=1)
     words[0] = words[0].title()
-    return ' '.join(words)
+    return " ".join(words)
 
 
 def is_supported(support_entries):
     if not support_entries:
         return None
 
-    latest = support_entries[0] if isinstance(support_entries, list) else support_entries
-    return latest.get('version_added') and not latest.get('version_removed')
+    latest = (
+        support_entries[0] if isinstance(support_entries, list) else support_entries
+    )
+    return latest.get("version_added") and not latest.get("version_removed")
 
 
 def create_surfly_support_entries(
@@ -98,56 +99,69 @@ def create_surfly_support_entries(
     else:
         surfly_support_entries = copy.deepcopy(native_support_entries)
 
-        if limitations.strip() or icf_limitations.strip() or icf_support not in (Support.SUPPORTED, Support.EXPECTED):
-            xs = surfly_support_entries if isinstance(surfly_support_entries, list) else [surfly_support_entries]
+        if (
+            limitations.strip()
+            or icf_limitations.strip()
+            or icf_support not in (Support.SUPPORTED, Support.EXPECTED)
+        ):
+            xs = (
+                surfly_support_entries
+                if isinstance(surfly_support_entries, list)
+                else [surfly_support_entries]
+            )
             for support_entry in xs:
                 if is_supported(support_entry):
-                    support_entry['partial_implementation'] = True
+                    support_entry["partial_implementation"] = True
 
-    for n in create_support_notes(support, limitations, icf_support, icf_limitations, extra_note):
+    for n in create_support_notes(
+        support, limitations, icf_support, icf_limitations, extra_note
+    ):
         add_note(surfly_support_entries, n)
 
     return surfly_support_entries
 
-def create_support_notes(support, limitations, icf_support, icf_limitations, extra_note):
+
+def create_support_notes(
+    support, limitations, icf_support, icf_limitations, extra_note
+):
     if extra_note.strip():
         yield extra_note
 
     icf_notes = []
     if support != icf_support:
         if icf_support == Support.UNKNOWN:
-            icf_notes.append('unknown support.')
+            icf_notes.append("unknown support.")
         elif icf_support == Support.SUPPORTED:
-            icf_notes.append('supported.')
+            icf_notes.append("supported.")
         elif icf_support == Support.EXPECTED:
-            icf_notes.append('expected to work.')
+            icf_notes.append("expected to work.")
         elif icf_support == Support.TODO:
-            icf_notes.append('not yet supported.')
+            icf_notes.append("not yet supported.")
         elif icf_support == Support.NEVER:
-            icf_notes.append('cannot support due to a browser limitation.')
+            icf_notes.append("cannot support due to a browser limitation.")
     if icf_limitations.strip():
         icf_notes.append(icf_limitations)
     if icf_notes:
         icf_notes.insert(0, "<strong>Controlling another user's tab:</strong>")
-        yield ' '.join(icf_notes)
+        yield " ".join(icf_notes)
 
     notes = []
     if support == Support.NEVER:
-        notes.append('cannot support due to a browser limitation.')
+        notes.append("cannot support due to a browser limitation.")
     elif support == Support.EXPECTED:
-        notes.append('expected to work.')
+        notes.append("expected to work.")
     elif support == Support.UNKNOWN:
-        notes.append('unknown Surfly support.')
+        notes.append("unknown Surfly support.")
     elif icf_notes and support == Support.SUPPORTED:
-        notes.append('full Surfly support.')
+        notes.append("full Surfly support.")
     if limitations.strip():
         notes.append(limitations)
     if notes:
         if icf_notes:
             notes.insert(0, "<strong>Tab owner in control:</strong>")
-            yield ' '.join(notes)
+            yield " ".join(notes)
         else:
-            yield capitalize(' '.join(notes))
+            yield capitalize(" ".join(notes))
 
 
 def add_note(support_entry, new_note):
@@ -155,13 +169,13 @@ def add_note(support_entry, new_note):
         support_entry = support_entry[0]
 
     try:
-        notes = support_entry['notes']
+        notes = support_entry["notes"]
     except KeyError:
-        support_entry['notes'] = new_note
+        support_entry["notes"] = new_note
         return
 
     if isinstance(notes, str):
-        support_entry['notes'] = [new_note, notes]
+        support_entry["notes"] = [new_note, notes]
         return
 
     notes.insert(0, new_note)
@@ -173,25 +187,30 @@ def overlay_browsers(upstream_browsers, supported_browser_ids):
         yield (browser_id, upstream_browser)
 
         surfly_browser = upstream_browser.copy()
-        surfly_browser['name'] = f'Surfly on {upstream_browser["name"]}'
-        yield (f'surfly_{browser_id}', surfly_browser)
+        surfly_browser["name"] = f'Surfly on {upstream_browser["name"]}'
+        yield (f"surfly_{browser_id}", surfly_browser)
 
 
 def export(output_path, feature_data, browsers, feature_id=None):
     for k, subfeature_data in feature_data.items():
-        if k == '__compat':
+        if k == "__compat":
             out = dict(
                 browsers=browsers,
                 query=feature_id,
                 data=feature_data,
             )
-            feature_path = output_path / f'{feature_id}.json'
+            feature_path = output_path / f"{feature_id}.json"
             print(feature_path.name, file=sys.stderr)
-            with (output_path / f'{feature_id}.json').open('w') as f:
+            with (output_path / f"{feature_id}.json").open("w") as f:
                 json.dump(out, f, separators=",:")
 
         elif isinstance(subfeature_data, dict):
-            export(output_path, subfeature_data, browsers, feature_id=k if feature_id is None else f'{feature_id}.{k}')
+            export(
+                output_path,
+                subfeature_data,
+                browsers,
+                feature_id=k if feature_id is None else f"{feature_id}.{k}",
+            )
 
 
 try:
@@ -201,7 +220,7 @@ except IndexError:
 output_path.mkdir(parents=True, exist_ok=True)
 
 spec = bcd.download()
-all_browsers = spec.pop('browsers')
+all_browsers = spec.pop("browsers")
 browsers = dict(overlay_browsers(all_browsers, supported_browser_ids))
 overlay(spec, supported_browser_ids)
 export(output_path, spec, browsers)
